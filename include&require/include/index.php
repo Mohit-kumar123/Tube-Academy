673f10dd683b28a6b112fa1a529941f47ff73b59
Mohit Kumar <mk0733920@gmail.com>
2024-11-15 22:51:04 +0530

    Delete include&require directory

--- include&require/include/index.php
+++
@@ -1,9 +0,0 @@
-<!-- index.php -->
-<?php include 'header.php'; ?>
-
-<main>
-    <h2>Main Content of the Home Page</h2>
-    <p>This is the content of the home page.</p>
-</main>
-
-<?php include 'footer.php'; ?>
--- include&require/include_once/index.php
+++
@@ -1,9 +0,0 @@
-<!-- index.php -->
-<?php include_once 'header.php'; ?>
-
-<main>
-    <h2>Main Content of the Home Page</h2>
-    <p>This is the content of the home page.</p>
-</main>
-
-<?php include_once 'footer.php'; ?>
